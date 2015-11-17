429c1d5e8fba4355033e1ebe91247321adf22985
Conor Lee <conor_lee@rapid7.com>
2015-11-17 14:40:42 -0500

    First pass at OS Services rules (2.0-3.0)

---
+++ recipes/OS_Services.rb
@@ -0,0 +1,75 @@
+#2.1.1
+service 'telnet-server' do
+  action :remove
+end
+#2.1.2
+service 'telnet' do
+  action :remove
+end
+#2.1.3
+service 'rsh-server' do
+  action :remove
+end
+#2.1.4
+service 'rsh' do
+  action :remove
+end
+#2.1.5
+service 'ypbind' do
+  action :remove
+end
+#2.1.6
+service 'ypserv' do
+  action :remove
+end
+#2.1.7
+service 'tftp' do
+  action :remove
+end
+#2.1.8
+service 'tftp-server' do
+  action :remove
+end
+#2.1.9
+service 'talk' do
+  action :remove
+end
+#2.1.10
+service 'talk-server' do
+  action :remove
+end
+#2.1.11
+service 'xinetd' do
+  action :remove
+end
+#2.1.12
+service 'chargen-dgram' do
+  action [:stop, :disable]
+end
+#2.1.13
+service 'chargen-stream' do
+  action [:stop, :disable]
+end
+#2.1.14
+service 'daytime-dgram' do
+  action [:stop, :disable]
+end
+#2.1.15
+service 'daytime-stream' do
+  action [:stop, :disable]
+end
+#2.1.16
+service 'echo-dgram' do
+  action [:stop, :disable]
+end
+#2.1.17
+service 'echo-stream' do
+  action [:stop, :disable]
+end
+#2.1.18
+service 'tcpmux-server' do
+  action [:stop, :disable]
+end
+
+
+
